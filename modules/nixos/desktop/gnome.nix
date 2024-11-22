6ec7f47581c28871fa2b3f251cc90c760c338c34
Marcin Kokoszka <mcocoshka@gmail.com>
2024-11-22 22:34:08 +0100

    Configute theme for Qt apps

--- modules/nixos/desktop/gnome.nix
+++ modules/nixos/desktop/gnome.nix
@@ -18,6 +18,12 @@ in {
     services.xserver.displayManager.gdm.enable = true;
     services.xserver.desktopManager.gnome.enable = true;
 
+    qt = {
+      enable = true;
+      platformTheme = "gnome";
+      style = "adwaita-dark";
+    };
+
     environment.gnome.excludePackages = with pkgs; [
       gnome-contacts
       gnome-music
